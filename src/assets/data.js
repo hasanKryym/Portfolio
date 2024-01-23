722638c0cd1461c9fe0036488d38fca480ee91c7
hasanKryym <hasan.kryym@gmail.com>
2024-01-23 12:16:22 +0200

    added shopy, and mazad

--- src/assets/data.js
+++ src/assets/data.js
@@ -1,79 +1,93 @@
 const skillsData = [
   {
-    image: 'HTML_logo.jpg',
-    title: 'HTML',
+    image: "HTML_logo.jpg",
+    title: "HTML",
   },
   {
-    image: 'CSS_logo.jpg',
-    title: 'CSS',
+    image: "CSS_logo.jpg",
+    title: "CSS",
   },
   {
-    image: 'Javascript_logo.jpg',
-    title: 'Javascript',
+    image: "Javascript_logo.jpg",
+    title: "Javascript",
   },
 
   {
-    image: 'React_logo.jpg',
-    title: 'React JS',
+    image: "React_logo.jpg",
+    title: "React JS",
   },
   {
-    image: 'Node_logo.jpg',
-    title: 'Node JS',
+    image: "Node_logo.jpg",
+    title: "Node JS",
   },
   {
-    image: 'Express_logo.jpg',
-    title: 'Express JS',
+    image: "Express_logo.jpg",
+    title: "Express JS",
   },
   {
-    image: 'Postgresql_logo.jpg',
-    title: 'Postgresql',
+    image: "Postgresql_logo.jpg",
+    title: "Postgresql",
   },
   {
-    image: 'MongoDB_logo.jpg',
-    title: 'MongoDB',
+    image: "MongoDB_logo.jpg",
+    title: "MongoDB",
   },
 ];
 
 const projectsData = [
   {
-    image: 'blogsite.png',
-    title: 'Blog Site',
+    image: "blogsite.png",
+    title: "Blog Site",
     description:
-      'A dynamic blog website, where users can create, read, like, and comment on posts. The website provides an interactive platform for sharing ideas and engaging with the community',
-    live_link: 'https://dailyblogsite.netlify.app/',
-    github_link: 'https://github.com/hasanKryym/Blogsite',
+      "A dynamic blog website, where users can create, read, like, and comment on posts. The website provides an interactive platform for sharing ideas and engaging with the community",
+    live_link: "https://dailyblogsite.netlify.app/",
+    github_link: "https://github.com/hasanKryym/Blogsite",
   },
   {
-    image: 'gamingWebsite.png',
-    title: 'Gaming Addict',
+    image: "mazad.png",
+    title: "Mazad",
     description:
-      'A gaming website that provides connections between gamers and provides them with the latest game updates',
-    live_link: 'https://gaming-addictt.netlify.app/',
-    github_link: 'https://github.com/hasanKryym/gaming-Addict',
+      "This project is a bidding system that allows users to participate in auctions, place bids on products, and view their bidding history. It provides functionalities for creating new bids, managing products, and tracking user interactions with the bidding platform.",
+    github_link: "https://github.com/hasanKryym/Mazad",
   },
   {
-    image: 'transactions.png',
-    title: 'Store Transactions',
+    image: "shopy.png",
+    title: "shopy",
     description:
-      'Ultimate solution for store transactions management. This web application empowers businesses to efficiently track transactions, products, and customers. Simplify your operations, stay organized, and gain valuable insights.',
-    live_link: '',
-    github_link: 'https://github.com/hasanKryym/Store_transactions',
+      "An e-commerce website where the admin can add his products and keep track of them. Also, admins are able to track the delivery of the order and change its status, whether it is delivered or canceled.",
+    github_link: "https://github.com/hasanKryym/shopy",
   },
   {
-    image: 'fitnessZone.png',
-    title: 'Fitness Zone',
+    image: "gamingWebsite.png",
+    title: "Gaming Addict",
     description:
-      'This website provides a comprehensive platform to help you achieve your health and wellness goals. Explore a wealth of workout routines, nutrition tips, and expert advice tailored to your fitness journey.',
-    live_link: 'https://fitnesss-zone.netlify.app/',
-    github_link: 'https://github.com/hasanKryym/Fitness-Zone',
+      "A gaming website that provides connections between gamers and provides them with the latest game updates",
+    live_link: "https://gaming-addictt.netlify.app/",
+    github_link: "https://github.com/hasanKryym/gaming-Addict",
   },
   {
-    image: 'busSchedule.png',
-    title: 'Bus Schedule',
+    image: "transactions.png",
+    title: "Store Transactions",
     description:
-      'This user-friendly web application allows you to effortlessly browse bus schedules, helping you find the optimal travel time for your journey.',
-    live_link: '',
-    github_link: 'https://github.com/hasanKryym/Bus_Schedule',
+      "Ultimate solution for store transactions management. This web application empowers businesses to efficiently track transactions, products, and customers. Simplify your operations, stay organized, and gain valuable insights.",
+    live_link: "",
+    github_link: "https://github.com/hasanKryym/Store_transactions",
+  },
+  {
+    image: "fitnessZone.png",
+    title: "Fitness Zone",
+    description:
+      "This website provides a comprehensive platform to help you achieve your health and wellness goals. Explore a wealth of workout routines, nutrition tips, and expert advice tailored to your fitness journey.",
+    live_link: "https://fitnesss-zone.netlify.app/",
+    github_link: "https://github.com/hasanKryym/Fitness-Zone",
+  },
+  {
+    image: "busSchedule.png",
+    title: "Bus Schedule",
+    description:
+      "This user-friendly web application allows you to effortlessly browse bus schedules, helping you find the optimal travel time for your journey.",
+    live_link: "",
+    github_link: "https://github.com/hasanKryym/Bus_Schedule",
   },
 ];
 
